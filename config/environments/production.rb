b54b07a833e6ef3c8435c0c9c448b6834422fde6
izmipo <2fromage2@gmail.com>
2017-09-29 18:00:35 +0900

    Add account activation2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: https://tranquil-tundra-46549.herokuapp.com }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
